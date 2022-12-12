f27c91a8670c74201b9f220150558a242f91280e
Ruslan Shlinchak <ruslanshlinchak@gmail.com>
2022-12-12 21:39:56 +0200

    fix: exchange controller

--- backend/src/controllers/exchange.controller.js
+++ backend/src/controllers/exchange.controller.js
@@ -1,10 +1,10 @@
 import { exchangeService } from "../services" 
 
 export const ExchangeController = async (req, res, next) => {
-  const handlersMap = async (req, res, next) => {
+  const handlersMap = async () => {
     try {
       const result = await exchangeService.convert(req.body);
-      res.res(result);
+      res.send(result);
     } catch (error) {
       next(error);
     }
